31ace38b21376c4d23e092a8ef452cb39161aa77
Rafał Maciak <rafalmaciak@gmail.com>
2023-02-02 16:28:34 +0100

    pact verification using Spock and groovy

---
+++ pact/payment-service/src/test/groovy/dev/rmaciak/payment/ContractVerificationSpec.groovy
@@ -0,0 +1,53 @@
+package dev.rmaciak.payment
+
+import au.com.dius.pact.provider.ConsumerInfo
+import au.com.dius.pact.provider.ProviderInfo
+import au.com.dius.pact.provider.ProviderVerifier
+import au.com.dius.pact.provider.VerificationResult
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class ContractVerificationSpec extends Specification {
+
+    @LocalServerPort
+    private int port
+
+    @Shared
+    ProviderInfo serviceProvider
+
+    ProviderVerifier verifier
+
+    def setupSpec() {
+        serviceProvider = new ProviderInfo("payment-service")
+        serviceProvider.hasPactsFromPactBroker("http://localhost:9292")
+    }
+
+    def setup() {
+        verifier = new ProviderVerifier()
+        serviceProvider.port = port
+    }
+
+    def 'verify pact with #consumer'() {
+        expect:
+        verifyConsumerPact(consumer) instanceof VerificationResult.Ok
+
+        where:
+        consumer << serviceProvider.consumers
+    }
+
+    private VerificationResult verifyConsumerPact(ConsumerInfo consumer) {
+        verifier.initialiseReporters(serviceProvider)
+        def result = verifier.runVerificationForConsumer([:], serviceProvider, consumer)
+
+        if (result instanceof VerificationResult.Failed) {
+            verifier.displayFailures([result])
+        }
+
+        result
+    }
+}
