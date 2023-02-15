f75fa24a1086da7d68b036aeac5118075569aaf1
Rafał Maciak <rafalmaciak@gmail.com>
2023-02-15 12:05:29 +0100

    Cleaning

--- spring-cloud-contract/rest-api/payment-service/src/main/java/com/rmaciak/payment/domain/NonPositivePaymentQuotaException.java
+++ spring-cloud-contract/rest-api/payment-service/src/main/java/com/rmaciak/payment/domain/NonPositivePaymentQuotaException.java
@@ -1,9 +1,4 @@
 package com.rmaciak.payment.domain;
 
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-
-//@ResponseStatus(value = UNPROCESSABLE_ENTITY)
 public class NonPositivePaymentQuotaException extends RuntimeException {
 }
