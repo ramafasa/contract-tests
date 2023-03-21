0f68fd22bb3a1dcea622e44af1e56428f2f9ce3e
Rafał Maciak <rafalmaciak@gmail.com>
2023-03-21 13:54:29 +0100

    Consumer tests of Kafka integration with Pact

--- pact/kafka-events/payment-service/src/main/java/dev/rmaciak/payment/PaymentApplication.java
+++ pact/kafka-events/payment-service/src/main/java/dev/rmaciak/payment/PaymentApplication.java
@@ -1,12 +1,18 @@
 package dev.rmaciak.payment;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.converter.RecordMessageConverter;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 
+@SpringBootApplication
 class PaymentApplication {
 
     public static void main(String[] args) {
@@ -21,5 +27,11 @@ class PaymentApplication {
     @Bean
     public RecordMessageConverter converter() {
         return new StringJsonMessageConverter();
+    }
+
+    @Bean
+    public ProducerFactory kafkaProducerFactory(KafkaProperties properties) {
+        ByteArraySerializer serializer = new ByteArraySerializer();
+        return new DefaultKafkaProducerFactory<>(properties.buildProducerProperties(), serializer, serializer);
     }
 }
--- pact/kafka-events/payment-service/src/test/groovy/dev/rmaciak/payment/EventHandlerSpec.groovy
+++ pact/kafka-events/payment-service/src/test/groovy/dev/rmaciak/payment/EventHandlerSpec.groovy
@@ -1,9 +1,73 @@
 package dev.rmaciak.payment
 
-
+import au.com.dius.pact.consumer.groovy.messaging.PactMessageBuilder
+import au.com.dius.pact.core.model.messaging.Message
+import dev.rmaciak.payment.domain.PaymentRepository
+import dev.rmaciak.payment.events.OrderCreatedEvent
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.KafkaContainer
+import org.testcontainers.spock.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.util.concurrent.PollingConditions
 
+import static dev.rmaciak.payment.domain.PaymentStatus.FINISHED
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
+
+@SpringBootTest(classes = [PaymentApplication], webEnvironment = NONE)
+@Testcontainers
+@ActiveProfiles("test")
 class EventHandlerSpec extends Specification {
 
+    @Shared
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
 
+    @Autowired
+    private KafkaTemplate<String, byte[]> kafkaTemplate
+
+    @Autowired
+    private PaymentRepository paymentRepository
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        kafka.start()
+        registry.add("spring.kafka.bootstrap-servers", () -> kafka.getBootstrapServers())
+    }
+
+    def "should initiate payment on OrderCreatedEvent"() {
+        given:
+        def eventFlow = new PactMessageBuilder().call {
+            serviceConsumer 'payment-service'
+            hasPactWith 'order-service'
+
+            expectsToReceive 'OrderCreatedEvent'
+            withContent(contentType: 'application/json') {
+                eventId uuid()
+                occurredAt datetime()
+
+                orderId uuid()
+                accountId uuid()
+                total decimal()
+                isPaid true
+            }
+        }
+
+        when:
+        eventFlow.run { Message message ->
+            kafkaTemplate.send('OrderCreated', message.contentsAsBytes())
+        }
+
+        then:
+        new PollingConditions(timeout: 10, initialDelay: 1.5, factor: 1.25).eventually {
+            def payments = paymentRepository.getAll()
+            assert payments.size() == 1
+            assert payments[0].paymentStatus() == FINISHED
+        }
+    }
 }
