dc16660a3a989ecc0915e301ab68621126dd7832
ramafasa <rafalmaciak@gmail.com>
2023-06-19 13:42:05 +0200

    Presentation mode

---
+++ pact/rest-api/payment-service/src/test/java/dev/rmaciak/payment/ContractVerificationTest.java
@@ -0,0 +1,25 @@
+package dev.rmaciak.payment;
+
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
+@Provider("payment-service")
+@PactBroker
+class ContractVerificationTest {
+
+    @TestTemplate
+    @ExtendWith(PactVerificationSpringProvider.class)
+    void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+}
